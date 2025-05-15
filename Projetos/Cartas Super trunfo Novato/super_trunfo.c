ffaf937328e52993ef8eb7c78e7ccae8140aacf1
David-Policarpo <policarpo1123@gmail.com>
2025-05-15 03:00:59 +0000

    Pending changes exported from your codespace

---
+++ Projetos/Cartas Super trunfo Novato/super_trunfo.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+int main(){
+
+    char estado1, estado2; // estado1 = Primeira Carta / estado2 = Segunda Carta
+    char cod1[3], cod2[3] ; // Código da Carta 1 / Código da Carta 2 
+    char cidade1[30], cidade2[30]; // Nome da Cidade 1 / Nome da Cidade 2
+    int populacao1, populacao2; // População da Cidade 1 / População da Cidade 2
+    int turistico1, turistico2; // Número de pontos Turísticos da cidade 1 / Número de pontos Turísticos da cidade 2
+    float area1, area2; // Área em km² cidade 1 / Área em km² cidade 2
+    float pib1, pib2; // PIB (Produto Interno Bruto) cidade 1 / PIB (Produto Interno Bruto) cidade 2
+
+
+    // Iremos fazer uma carta de cada vez.
+
+    printf("Digite a Letra do Estado da Primeira Carta (EX: A - H): \n"); // Uma letra entre A e H.
+    scanf("%s", &estado1);
+
+    printf("Digite os dois dígitos Segunda Carta (EX: A02): \n");
+    scanf("%s", &cod1); // Concertar esse código que está puxando os 2 ao mesmo tempo.
+
+    printf("Digite o Nome da cidade (EX: Rio de Janeiro): \n");
+    scanf(" %[^\n]", cidade1);   // Pesquisando um pouco descobri que usando esse especificador dá pra usar nomes compostos para as cidades com o Scanf.
+
+    printf("Digite o número de habitantes da cidade (EX: 12345): \n"); // Vou melhorar colocando a pontuação certinha de mil, milhão e etc...
+    scanf("%i", &populacao1);
+
+    printf("Digite a Área da cidade em quilômetros quadrados (EX: 1200.02): \n"); // Adicionar KM² no terminal de saída.
+    scanf("%f", &area1);
+
+    printf("Digite o PIB (EX: 699.00): \n");
+    scanf("%f", &pib1);
+
+    printf("Digite o número de pontos turísticos (EX: 20): \n");
+    scanf("%i", &turistico1);
+
+        // Iremos pra segunda carta agora...
+
+
+        printf("\nDigite a Letra do Estado da Segunda Carta (EX: A - H): \n"); // Uma letra entre A e H.
+        scanf("%s", &estado2);
+
+        printf("Digite os dois dígitos Segunda Carta (EX: A02): \n");
+         scanf("%s", &cod2);
+
+        printf("Digite o Nome da cidade (EX: Rio de Janeiro): \n");
+        scanf(" %[^\n]", cidade2);   // Pesquisando um pouco descobri que usando esse especificador dá pra usar nomes compostos para as cidades com o Scanf.
+
+        printf("Digite o número de habitantes da cidade (EX: 12345): \n"); // Vou melhorar colocando a pontuação certinha de mil, milhão e etc...
+        scanf("%i", &populacao2);
+
+        printf("Digite a Área da cidade em quilômetros quadrados (EX: 1200.02): \n"); // Adicionar KM² no terminal de saída.
+        scanf("%f", &area2);
+
+        printf("Digite o PIB (EX: 699.00): \n");
+        scanf("%f", &pib2);
+
+        printf("Digite o número de pontos turísticos (EX: 20): \n");
+        scanf("%i", &turistico2);
+    
+
+            // Agora vamos puxar os dados digitados na ordem que foi pedida... Colocando em ordem para que as duas cartas apareçam uma após a outra.
+            // Requisição da primeira carta
+            printf("\nCarta 1:\n");
+            printf("Estado: %c\n", estado1);
+            printf("Código: %s\n", cod1);  
+            printf("Nome da Cidade: %s\n", cidade1);
+            printf("População: %i\n", populacao1);
+            printf("Área: %.2f KM²\n", area1);
+            printf("PIB: %.2f Bilhões\n", pib1);
+            printf("Número de Pontos Turísticos: %i\n", turistico1);
+
+                // Requisição da Segunda carta
+
+                printf("\nCarta 2:\n");
+                printf("Estado: %c\n", estado2);
+                printf("Código: %s\n", cod2); 
+                printf("Nome da Cidade: %s\n", cidade2);
+                printf("População: %i\n", populacao2);
+                printf("Área: %.2f KM²\n", area2);
+                printf("PIB: %.2f Bilhões\n", pib2);
+                printf("Número de Pontos Turísticos: %i\n", turistico2);
+
+return 0;
+
+}
+
+// Fim???
