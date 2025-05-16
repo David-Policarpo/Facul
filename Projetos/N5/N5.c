32afc1c59547125a38fbd80e6d7094eb027a68ed
David-Policarpo <policarpo1123@gmail.com>
2025-05-16 23:40:51 +0000

    committ N5

---
+++ Projetos/N5/N5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int b = 20;
+
+
+    
+
+    printf("A > B: %d\n", a > b);
+    printf("A < B: %d\n", a < b);
+    printf("A == B: %d\n", a == b);
+    printf("A != B: %d\n", a != b);
+
+    return 0;
+
+
+}
