64a9284af026ffae55c3429ca7a0d0e506c9327f
David-Policarpo <policarpo1123@gmail.com>
2025-05-18 12:26:16 +0000

    Com

---
+++ Projetos/Projeto Logica/N1/N1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int idade;
+
+    idade = 15;
+    
+    if(idade >= 18){;
+
+    printf("Você é maior de idade\n");
+    printf("Dentro if\n");
+    }
+    
+    printf("Fora If\n");
+}
