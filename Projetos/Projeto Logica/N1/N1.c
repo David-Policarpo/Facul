30919033c4a2c2e2e291389dc26830b6ca9d759c
David-Policarpo <policarpo1123@gmail.com>
2025-05-18 13:14:58 +0000

    att

--- Projetos/Projeto Logica/N1/N1.c
+++ Projetos/Projeto Logica/N1/N1.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 int main(){
     int idade1, idade2;
+    int numero = 10;
 
     idade1 = 15;
-    idade2 = 15;
+    idade2 = 32;
     
     if(idade1 != idade2){
 
     printf("As idades são diferentes.\n");
     }
     else {
-        printf("As idades são iguais.\n");
+        printf("As idades são iguais.\n");}
 
-}
+   
+    }
+            
+
+
+
+
+   
+
+          
     
-}
+
