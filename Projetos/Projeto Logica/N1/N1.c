a8f9109ee78c9efcacf45f72576f9a4cfacbb282
David-Policarpo <policarpo1123@gmail.com>
2025-05-18 13:02:33 +0000

    att

--- Projetos/Projeto Logica/N1/N1.c
+++ Projetos/Projeto Logica/N1/N1.c
@@ -3,13 +3,15 @@ int main(){
     int idade1, idade2;
 
     idade1 = 15;
-    idade2 = 32;
+    idade2 = 15;
     
     if(idade1 != idade2){
 
     printf("As idades são diferentes.\n");
-   
     }
-    
+    else {
+        printf("As idades são iguais.\n");
+
+}
     
 }
