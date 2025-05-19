d5baa4cf8b7081ee30403f35eb52d25367808b10
David-Policarpo <policarpo1123@gmail.com>
2025-05-19 23:30:54 +0000

    N3

---
+++ Projetos/Projeto Logica/N3/N3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int idade = 20;
+    float altura = 1.69;
+   
+    if (idade >= 18 && altura >= 1.70) 
+    { printf("Ele pode entrar no brinquedo.\n");
+       }else{
+        printf("Ele não pode entrar no brinquedo. \n");
+       }
+    
+
+}
